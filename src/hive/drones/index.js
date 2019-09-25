ea72961a4c4793782ed06e07daa839632b2aceaa
Megan <megan.lkm@gmail.com>
2019-09-25 13:15:51 -0500

    implement kinesis event writer

--- src/hive/drones/index.js
+++ src/hive/drones/index.js
@@ -1,14 +1,21 @@
 const axios = require("axios")
 
-const PROTON_PACK_STREAM_URI = process.env.PROTON_PACK_STREAM_URI
+const AWS = require('aws-sdk')
+var client = new AWS.Kinesis();
+
+const BEEHIVE_STREAM = process.env.BEEHIVE_STREAM ? process.env.BEEHIVE_STREAM : "beehive_stream"
+const BEEHIVE_PARTITION_KEY = process.env.BEEHIVE_PARTITION_KEY ? process.env.BEEHIVE_PARTITION_KEY : "beehive_partition_key"
+
 
 class Event {
-    constructor(topic, type, id, activity, ts) {
+    constructor(topic, type, id, activity, ts, stream_name, partition_key) {
         this.topic = topic
         this.type = type
         this.id = id
         this.activity = activity
         this.ts = ts ? ts : new Date().toISOString()
+        this.stream_name = stream_name ? stream_name : BEEHIVE_STREAM
+        this.partition_key = partition_key ? partition_key : BEEHIVE_PARTITION_KEY
     }
 
     json() {
@@ -22,9 +29,19 @@ class Event {
     }
 }
 
+
 exports.sendEvent = async function(event) {
-    var response = await axios.post(PROTON_PACK_STREAM_URI, event.json())
-    return response
+    client.putRecord({
+        PartitionKey: event.partition_key,
+        StreamName: event.stream_name,
+        Data: JSON.stringify(event.json())
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log(data);
+        }
+    });
 }
 
 exports.Event = Event
