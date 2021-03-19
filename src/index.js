79d49f986ab44f8d706706f3ef7fe8ca45759921
optimuspaul <paul@decoursey.net>
2021-03-19 09:03:37 -0500

    forgot to export BeehivePlugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const {BeehiveResolvers, BeehiveDirectives, BeehiveTypeDefs, ensureDatabase, hivePg} = require("./hive")
+const {BeehiveResolvers, BeehiveDirectives, BeehiveTypeDefs, ensureDatabase, hivePg, BeehivePlugin} = require("./hive")
 
 exports.BeehiveResolvers = BeehiveResolvers
 exports.BeehiveDirectives = BeehiveDirectives
 exports.BeehiveTypeDefs = BeehiveTypeDefs
 exports.ensureDatabase = ensureDatabase
 exports.hivePg = hivePg
+exports.BeehivePlugin = BeehivePlugin
